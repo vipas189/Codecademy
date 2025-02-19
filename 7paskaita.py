690af4767d307bad320b2e62b34496ce9223ea1a
vipas189 <rokx987@gmail.com>
2025-02-20 01:25:48 +0200

    haha2

--- 7paskaita.py
+++ 7paskaita.py
@@ -13,9 +13,8 @@ Readability counts.
 Special cases aren't special enough to break the rules.
 Although practicality beats purity.
 Errors should never pass silently.
-Unless explicitly silenced.
 In the face of ambiguity, refuse the temptation to guess.
-There should be one—and preferably only one—obvious way to do it.
+asdfsadf
 Although that way may not be obvious at first unless you're Dutch.
 Now is better than never.
 Although never is often better than right now.
